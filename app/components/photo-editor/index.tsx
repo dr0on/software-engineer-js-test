1942559c5c0cc4f08e8003725c50603093720d98
Andriy.Osadchuk <andriy_osadchuk@epam.com>
2023-01-04 15:27:35 +0200

    formated by Prettier

--- app/components/photo-editor/index.tsx
+++ app/components/photo-editor/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export const PhotoEditor = () => {
 	const [image, setImage] = useState<HTMLImageElement>();
-  const [options, setOptions] = useState({x: 0, y: 0, scale: 1});
+	const [options, setOptions] = useState({ x: 0, y: 0, scale: 1 });
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
@@ -11,8 +11,14 @@ export const PhotoEditor = () => {
 			const canvas = canvasRef.current;
 			const ctx = canvas.getContext('2d');
 
-      ctx?.clearRect(0, 0, canvas.width, canvas.height);
-      ctx?.drawImage(image, options.x, options.y, image.naturalWidth * options.scale, image.naturalHeight * options.scale);
+			ctx?.clearRect(0, 0, canvas.width, canvas.height);
+			ctx?.drawImage(
+				image,
+				options.x,
+				options.y,
+				image.naturalWidth * options.scale,
+				image.naturalHeight * options.scale
+			);
 		}
 	}, [image, options.x, options.y, options.scale]);
 
@@ -20,13 +26,13 @@ export const PhotoEditor = () => {
 		// get all selected Files
 		const files = e.target.files as FileList;
 		let file;
-		for ( let i = 0; i < files.length; ++i ) {
-			file = files[ i ];
+		for (let i = 0; i < files.length; ++i) {
+			file = files[i];
 			// check if file is valid Image (just a MIME check)
-			switch ( file.type ) {
-				case "image/jpeg":
-				case "image/png":
-				case "image/gif":
+			switch (file.type) {
+				case 'image/jpeg':
+				case 'image/png':
+				case 'image/gif':
 					// read Image contents from file
 					const reader = new FileReader();
 					reader.onload = (e: ProgressEvent<FileReader>) => {
@@ -34,111 +40,172 @@ export const PhotoEditor = () => {
 						const img = new Image();
 						img.src = reader.result as string;
 						img.onload = () => {
-              if (canvasRef.current) {
-                const editorCanvas = canvasRef.current;
-                let scaleFactor = Math.max(editorCanvas?.width / img.width, editorCanvas?.height / img.height);
-                
-                // Finding the new width and height based on the scale factor
-                let newWidth = img.width * scaleFactor;
-                let newHeight = img.height * scaleFactor;
-                
-                // get the top left position of the image
-                // in order to center the image within the canvas
-                let x = (editorCanvas.width / 2) - (newWidth / 2);
-                let y = (editorCanvas.height / 2) - (newHeight / 2);
+							if (canvasRef.current) {
+								const editorCanvas = canvasRef.current;
+								let scaleFactor = Math.max(
+									editorCanvas?.width / img.width,
+									editorCanvas?.height / img.height
+								);
 
-                setOptions({x, y, scale: scaleFactor});
-                setImage(img);
-              }
-            }
+								// Finding the new width and height based on the scale factor
+								let newWidth = img.width * scaleFactor;
+								let newHeight = img.height * scaleFactor;
+
+								// get the top left position of the image
+								// in order to center the image within the canvas
+								let x = editorCanvas.width / 2 - newWidth / 2;
+								let y = editorCanvas.height / 2 - newHeight / 2;
+
+								setOptions({ x, y, scale: scaleFactor });
+								setImage(img);
+							}
+						};
 					};
-					reader.readAsDataURL( file );
+					reader.readAsDataURL(file);
 					// process just one file
 					return;
 			}
 		}
 	};
 
-  const handleIncreaseZoomClick = () => {
-    const {x, y, scale} = calculateZoomOptions(image, 1.1);
+	const handlePhotoImport = async (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files as FileList;
+		let file: Blob;
+		for (let i = 0; i < files.length; ++i) {
+			file = files[i];
+			switch (file.type) {
+				case 'application/json':
+					const reader = new FileReader();
+					reader.onload = (e: ProgressEvent<FileReader>) => {
+						reader.readAsText(file);
+						reader.onload = function () {
+							const configs = JSON.parse(reader.result as string);
+							console.log('configs: ', configs);
 
-    setOptions({x, y, scale});
-  }
+							const img = new Image();
+							img.src = configs.canvas.photo.src;
 
-  const handleDecreaseZoomClick = () => {
-    const {x, y, scale} = calculateZoomOptions(image, 0.9);
+							setImage(img);
+						};
 
-    setOptions({x, y, scale});
-  }
+						// const img = new Image();
+						// img.src = reader.result as string;
+						// img.onload = () => {
+						//   if (canvasRef.current) {
+						//     const editorCanvas = canvasRef.current;
+						//     let scaleFactor = Math.max(editorCanvas?.width / img.width, editorCanvas?.height / img.height);
 
-  const calculateZoomOptions = (img: any, scale: number) => {
-    const currentWidth = img.width * options.scale;
-    const currentHeight = img.height * options.scale;
-    
-    const newWidth = currentWidth * scale;
-    const newHeight = currentHeight * scale;
-    
-    const xOffset = (newWidth - currentWidth) / 2;
-    const yOffset = (newHeight - currentHeight) / 2;
+						//     // Finding the new width and height based on the scale factor
+						//     let newWidth = img.width * scaleFactor;
+						//     let newHeight = img.height * scaleFactor;
 
-    return {x: options.x - xOffset, y: options.y - yOffset, scale: options.scale * scale};
-  }
+						//     // get the top left position of the image
+						//     // in order to center the image within the canvas
+						//     let x = (editorCanvas.width / 2) - (newWidth / 2);
+						//     let y = (editorCanvas.height / 2) - (newHeight / 2);
 
-  const handleTopClick = () => {
-    setOptions({x: options.x, y: options.y - 10, scale: options.scale});
-  }
+						//     setOptions({x, y, scale: scaleFactor});
+						//     setImage(img);
+						//   }
+						// }
+					};
+					reader.readAsDataURL(file);
+					// process just one file
+					return;
+			}
+		}
+	};
 
-  const handleBottomClick = () => {
-    setOptions({x: options.x, y: options.y + 10, scale: options.scale});
-  }
+	const handleIncreaseZoomClick = () => {
+		const { x, y, scale } = calculateZoomOptions(image, 1.1);
 
-  const handleRightClick = () => {
-    setOptions({x: options.x + 10, y: options.y, scale: options.scale});
-  }
+		setOptions({ x, y, scale });
+	};
 
-  const handleLeftClick = () => {
-    setOptions({x: options.x - 10, y: options.y, scale: options.scale});
-  }
+	const handleDecreaseZoomClick = () => {
+		const { x, y, scale } = calculateZoomOptions(image, 0.9);
 
-  const handleExportClick = () => {
-    const width: number = image?.width || 0;
-    const height: number = image?.height || 0;
-    const imgEncoded: string = canvasRef.current?.toDataURL() as string;
+		setOptions({ x, y, scale });
+	};
 
-    const settings = getImageSettings({imgEncoded, width, height, options });
+	const calculateZoomOptions = (img: any, scale: number) => {
+		const currentWidth = img.width * options.scale;
+		const currentHeight = img.height * options.scale;
 
-    saveSettingsFile(settings);
-  }
+		const newWidth = currentWidth * scale;
+		const newHeight = currentHeight * scale;
 
-  const getImageSettings = (
-    {imgEncoded = "", width, height, options}:
-    {imgEncoded: string, width: number, height: number, options:any }
-  ) => {
-    const { x, y, scale } = options;
+		const xOffset = (newWidth - currentWidth) / 2;
+		const yOffset = (newHeight - currentHeight) / 2;
 
-    return `{
-      "canvas": {
-        "width": ${width},
-        "height": ${height},
-        "photo" : {
-          "id": "image-configuration.json",
-          "src": ${imgEncoded},
-          "width": ${width * scale},
-          "height": ${height * scale},
-          "x": ${x},
-          "y": ${y}
-        }
-      }
-    }`;
-  }
+		return {
+			x: options.x - xOffset,
+			y: options.y - yOffset,
+			scale: options.scale * scale,
+		};
+	};
 
-  const saveSettingsFile = (settings: string, fileName = "image-configuration.json") => {
-    const element = document.createElement("a");
-    const file = new Blob([settings], {type: "application/json"});
-    element.href = URL.createObjectURL(file);
-    element.download = fileName;
-    element.click();
-  }
+	const handleTopClick = () => {
+		setOptions({ x: options.x, y: options.y - 10, scale: options.scale });
+	};
+
+	const handleBottomClick = () => {
+		setOptions({ x: options.x, y: options.y + 10, scale: options.scale });
+	};
+
+	const handleRightClick = () => {
+		setOptions({ x: options.x + 10, y: options.y, scale: options.scale });
+	};
+
+	const handleLeftClick = () => {
+		setOptions({ x: options.x - 10, y: options.y, scale: options.scale });
+	};
+
+	const handleExportClick = () => {
+		const width: number = image?.width || 0;
+		const height: number = image?.height || 0;
+		const imgEncoded: string = canvasRef.current?.toDataURL() as string;
+
+		const settings = getImageSettings({ imgEncoded, width, height, options });
+
+		saveSettingsFile(JSON.stringify(settings));
+	};
+
+	const getImageSettings = ({
+		imgEncoded = '',
+		width,
+		height,
+		options,
+	}: {
+		imgEncoded: string;
+		width: number;
+		height: number;
+		options: any;
+	}) => ({
+		canvas: {
+			width,
+			height,
+			photo: {
+				id: 'image-configuration.json',
+				src: imgEncoded,
+				width: width * options.scale,
+				heigh: height * options.scale,
+				x: options.x,
+				y: options.y,
+			},
+		},
+	});
+
+	const saveSettingsFile = (
+		settings: string,
+		fileName = 'image-configuration.json'
+	) => {
+		const element = document.createElement('a');
+		const file = new Blob([settings], { type: 'application/json' });
+		element.href = URL.createObjectURL(file);
+		element.download = fileName;
+		element.click();
+	};
 
 	return (
 		<>
@@ -147,17 +214,26 @@ export const PhotoEditor = () => {
 				<label htmlFor="fileSelector">Upload Images</label>
 				<input type="file" id="fileSelector" onChange={handlePhotoUpload} />
 			</div>
-			<canvas ref={canvasRef} width={500} height={350} style={{border: '2px solid black'}} />
+			<div>
+				<label htmlFor="fileSelector">Import Photo Settings</label>
+				<input type="file" id="fileSelector" onChange={handlePhotoImport} />
+			</div>
+			<canvas
+				ref={canvasRef}
+				width={500}
+				height={350}
+				style={{ border: '2px solid black' }}
+			/>
 
-        <button onClick={handleIncreaseZoomClick}>+</button>
-        <button onClick={handleDecreaseZoomClick}>-</button>
+			<button onClick={handleIncreaseZoomClick}>+</button>
+			<button onClick={handleDecreaseZoomClick}>-</button>
 
-        <button onClick={handleTopClick}>Top</button>
-        <button onClick={handleBottomClick}>Bottom</button>
-        <button onClick={handleRightClick}>Right</button>
-        <button onClick={handleLeftClick}>Left</button>
+			<button onClick={handleTopClick}>Top</button>
+			<button onClick={handleBottomClick}>Bottom</button>
+			<button onClick={handleRightClick}>Right</button>
+			<button onClick={handleLeftClick}>Left</button>
 
-        <button onClick={handleExportClick}>Export</button>
+			<button onClick={handleExportClick}>Export</button>
 		</>
 	);
 };
