bc42040ef9630634bba5378b2d9e64087aa18c10
Andriy.Osadchuk <andriy_osadchuk@epam.com>
2023-01-05 21:03:58 +0200

    added unit tests for useCanvas hook

--- app/components/photo-editor/index.spec.ts
+++ app/components/photo-editor/index.spec.ts
@@ -2,55 +2,104 @@
  * @jest-environment jsdom
  */
 
+import React, { useEffect }  from 'react';
 import { toDataURL } from './settingsUtils';
 import 'jest-canvas-mock';
+
+import useCanvas, { ImageAction } from './useCanvas';
 
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D | null;
 
-describe('PhotoEditor', () => {
+// jest.mock('React', () => ({
+//   ...jest.requireActual('React'),
+//   useEffect: jest.fn(),
+// }));
 
+describe('PhotoEditor', () => {
   beforeEach(() => {
-    // jest.resetAllMocks();
     canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
     canvas.width = 400;
     canvas.height = 300;
-
-    // ctx?.setTransform(1, 2, 3, 4, 5, 6);
   });
 
   it('should encode Image to base64', () => {
     const image = new Image(400, 300);    
     image.src = 'picture.jpg';
 
-    // ctx?.drawImage(image, 0, 0, 400, 300);
     const encodedImg = toDataURL(image);
 
     expect(encodedImg).toEqual('data:image/png;base64,00');
-    // const events = ctx?.__getEvents();
-
-    // expect(events).toMatchSnapshot();
-
-    // canvas.toDataURL.mockReturnValueOnce(
-    //   'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-    // );
   });
 
-  it('should encode Image to base64', () => {
-    const image = new Image(400, 300);    
-    image.src = 'picture.jpg';
+  describe('useCanvas', () => {
+    it('should return default options', () => {
+      jest.spyOn(React, 'useState')
+        .mockImplementationOnce(() => [new Image(), () => null])
+        .mockImplementationOnce(() => [{ x: 0, y: 0, scale: 1 }, () => null]);
+      jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: canvas });
+      jest.spyOn(React, 'useEffect').mockImplementation(f => f());
 
-    // ctx?.drawImage(image, 0, 0, 400, 300);
-    const encodedImg = toDataURL(image);
+      const { options } = useCanvas();
 
-    expect(encodedImg).toEqual('data:image/png;base64,00');
-    // const events = ctx?.__getEvents();
+      expect(options).toEqual({ x: 0, y: 0, scale: 1 });
+    });
 
-    // expect(events).toMatchSnapshot();
+    it('should return Ref to canvas', () => {
+      jest.spyOn(React, 'useState')
+        .mockImplementationOnce(() => [new Image(), () => null])
+        .mockImplementationOnce(() => [{ x: 0, y: 0, scale: 1 }, () => null]);
+      jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: canvas });
+      jest.spyOn(React, 'useEffect').mockImplementation(f => f());
 
-    // canvas.toDataURL.mockReturnValueOnce(
-    //   'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-    // );
+      const { canvasRef } = useCanvas();
+
+      expect(canvasRef.current).toEqual(canvas);
+    });
+
+    it('should return image', () => {
+      jest.spyOn(React, 'useState')
+        .mockImplementationOnce(() => [new Image(200, 100), () => null])
+        .mockImplementationOnce(() => [{ x: 0, y: 0, scale: 1 }, () => null]);
+      jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: canvas });
+      jest.spyOn(React, 'useEffect').mockImplementation(f => f());
+
+      const { image } = useCanvas();
+
+      expect(image.width).toEqual(200);
+    });
+
+    it('should zoom in image', () => {
+      jest.spyOn(React, 'useState')
+        .mockImplementationOnce(() => [new Image(200, 100), () => null])
+        .mockImplementationOnce(() => [{ x: 0, y: 0, scale: 1 }, () => null]);
+      jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: canvas });
+      jest.spyOn(React, 'useEffect').mockImplementation(f => f());
+
+      const { setImageAction } = useCanvas();
+
+      setImageAction(ImageAction.ZoomIn);
+
+      const events = ctx?.__getEvents();
+
+      expect(events).toMatchSnapshot();
+    });
+
+    it('should move image to top', () => {
+      jest.spyOn(React, 'useState')
+        .mockImplementationOnce(() => [new Image(1000, 1000), () => null])
+        .mockImplementationOnce(() => [{ x: 0, y: -15, scale: 1 }, () => null]);
+      jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: canvas });
+      jest.spyOn(React, 'useEffect').mockImplementation(f => f());
+
+      const { setImageAction } = useCanvas();
+
+      setImageAction(ImageAction.Top);
+
+      const events = ctx?.__getEvents();
+
+      expect(events).toMatchSnapshot();
+    });
   });
 });
