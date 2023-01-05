fc5675502414d604a56b2047879b54c8c473bb67
Andriy.Osadchuk <andriy_osadchuk@epam.com>
2023-01-05 14:46:54 +0200

    setup test env

--- app/components/photo-editor/index.spec.ts
+++ app/components/photo-editor/index.spec.ts
@@ -1,9 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { toDataURL, saveSettingsFile } from './ImageSettingsUtils';
+// import { setupJestCanvasMock } from 'jest-canvas-mock';
+import 'jest-canvas-mock';
+
 describe('PhotoEditor', () => {
-  it('should ...', () => {
-      // arrange
 
-      // act
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
-      // assert
+  it('should encode Image to base64', () => {
+    const image = new Image();
+    const encodedImg = toDataURL(image);
+
+    // canvas.toDataURL.mockReturnValueOnce(
+    //   'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+    // );
+
+    console.log(encodedImg);
   });
 });
