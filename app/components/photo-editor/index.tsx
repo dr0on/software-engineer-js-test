fc479f2df6695bdf0f3e882ca8da5789db91f4fb
Andriy.Osadchuk <andriy_osadchuk@epam.com>
2023-01-05 13:17:20 +0200

    move adjust logic to custom hook

--- app/components/photo-editor/index.tsx
+++ app/components/photo-editor/index.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import useCanvas, { ImageAction } from './useCanvas';
 
 export const PhotoEditor = () => {
-  const [image, setImage] = useState<HTMLImageElement>(new Image());
-  const [options, setOptions] = useState({ x: 0, y: 0, scale: 1 });
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (image && canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-
-      ctx?.clearRect(0, 0, canvas.width, canvas.height);
-      ctx?.drawImage(
-        image,
-        options.x,
-        options.y,
-        image.naturalWidth * options.scale,
-        image.naturalHeight * options.scale
-      );
-    }
-  }, [image, options.x, options.y, options.scale]);
+  const { canvasRef, image, setImage, options, setOptions, setImageAction } = useCanvas();
 
   const handlePhotoUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files as FileList;
@@ -88,80 +70,6 @@ export const PhotoEditor = () => {
       // process just one file
       return;
     }
-  };
-
-  const handleIncreaseZoomClick = () => {
-    const { x, y, scale } = calculateZoomOptions(image, 1.1);
-
-    setOptions({ x, y, scale });
-  };
-
-  const handleDecreaseZoomClick = () => {
-    if (!isTopLimit() && !isBottomLimit() && !isLeftLimit() && !isRightLimit()) {
-      const { x, y, scale } = calculateZoomOptions(image, 0.9);
-
-      setOptions({ x, y, scale });
-    }
-  };
-
-  const calculateZoomOptions = (img: any, scale: number) => {
-    const currentWidth = img.width * options.scale;
-    const currentHeight = img.height * options.scale;
-
-    const newWidth = currentWidth * scale;
-    const newHeight = currentHeight * scale;
-
-    const xOffset = (newWidth - currentWidth) / 2;
-    const yOffset = (newHeight - currentHeight) / 2;
-
-    return {
-      x: options.x - xOffset,
-      y: options.y - yOffset,
-      scale: options.scale * scale,
-    };
-  };
-
-  const handleTopClick = () => {
-    const y = options.y + 10 > 0 ? 0 : options.y + 10;
-    setOptions({ x: options.x, y, scale: options.scale });
-  };
-
-  const isBottomLimit = () => {
-    const canvasHeight = canvasRef.current?.height || 0;
-    const scaledHeight = image.height * options.scale;
-
-    return options.y + scaledHeight - 10 < canvasHeight;
-  };
-
-  const isLeftLimit = () => {
-    const canvasWidth = canvasRef.current?.width || 0;
-    const scaledWidth = image.width * options.scale;
-
-    return options.x + scaledWidth - 10 < canvasWidth;
-  };
-
-  const isTopLimit = () => options.y + 10 > 0;
-
-  const isRightLimit = () => options.x + 10 > 0;
-
-  const handleBottomClick = () => {
-    const canvasHeight = canvasRef.current?.height || 0;
-    let y = isBottomLimit() ? canvasHeight - image.height * options.scale : options.y - 10;
-
-    setOptions({ x: options.x, y, scale: options.scale });
-  };
-
-  const handleRightClick = () => {
-    const x = options.x + 10 > 0 ? 0 : options.x + 10;
-
-    setOptions({ x, y: options.y, scale: options.scale });
-  };
-
-  const handleLeftClick = () => {
-    const canvasWidth = canvasRef.current?.width || 0;
-    let x = isLeftLimit() ? canvasWidth - image.width * options.scale : options.x - 10;
-
-    setOptions({ x, y: options.y, scale: options.scale });
   };
 
   const handleExportClick = () => {
@@ -237,20 +145,20 @@ export const PhotoEditor = () => {
 
         <div className="edit-panel">
           <div className="zoom-controls">
-            <button onClick={handleIncreaseZoomClick}>+</button>
-            <button onClick={handleDecreaseZoomClick}>-</button>
+            <button onClick={() => setImageAction(ImageAction.ZoomIn)}>+</button>
+            <button onClick={() => setImageAction(ImageAction.ZoomOut)}>-</button>
           </div>
           <div className="move-controls">
-            <button className="top" onClick={handleTopClick}>
+            <button className="top" onClick={() => setImageAction(ImageAction.Top)}>
               Top
             </button>
-            <button className="bottom" onClick={handleBottomClick}>
+            <button className="bottom" onClick={() => setImageAction(ImageAction.Down)}>
               Bottom
             </button>
-            <button className="right" onClick={handleRightClick}>
+            <button className="right" onClick={() => setImageAction(ImageAction.Right)}>
               Right
             </button>
-            <button className="left" onClick={handleLeftClick}>
+            <button className="left" onClick={() => setImageAction(ImageAction.Left)}>
               Left
             </button>
           </div>
---
+++ app/components/photo-editor/useCanvas.tsx
@@ -0,0 +1,155 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const enum ImageAction {
+  Top,
+  Down,
+  Left,
+  Right,
+  ZoomIn,
+  ZoomOut,
+}
+
+const useCanvas = () => {
+  const [image, setImage] = useState<HTMLImageElement>(new Image());
+  const [options, setOptions] = useState({ x: 0, y: 0, scale: 1 });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (image && canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      ctx?.drawImage(
+        image,
+        options.x,
+        options.y,
+        image.naturalWidth * options.scale,
+        image.naturalHeight * options.scale
+      );
+    }
+  }, [image, options.x, options.y, options.scale]);
+
+  const calculateZoomOptions = (img: any, scale: number) => {
+    const currentWidth = img.width * options.scale;
+    const currentHeight = img.height * options.scale;
+
+    const newWidth = currentWidth * scale;
+    const newHeight = currentHeight * scale;
+
+    const xOffset = (newWidth - currentWidth) / 2;
+    const yOffset = (newHeight - currentHeight) / 2;
+
+    return {
+      x: options.x - xOffset,
+      y: options.y - yOffset,
+      scale: options.scale * scale,
+    };
+  };
+
+  const isBottomLimit = () => {
+    const canvasHeight = canvasRef.current?.height || 0;
+    const scaledHeight = image.height * options.scale;
+
+    return options.y + scaledHeight - 10 < canvasHeight;
+  };
+
+  const isLeftLimit = () => {
+    const canvasWidth = canvasRef.current?.width || 0;
+    const scaledWidth = image.width * options.scale;
+
+    return options.x + scaledWidth - 10 < canvasWidth;
+  };
+
+  const isTopLimit = () => options.y + 10 > 0;
+
+  const isRightLimit = () => options.x + 10 > 0;
+
+  const setZoomIn = () => {
+    const { x, y, scale } = calculateZoomOptions(image, 1.1);
+
+    setOptions({ x, y, scale });
+  };
+
+  const setZoomOut = () => {
+    if (!isTopLimit() && !isBottomLimit() && !isLeftLimit() && !isRightLimit()) {
+      const { x, y, scale } = calculateZoomOptions(image, 0.9);
+
+      setOptions({ x, y, scale });
+    }
+  };
+
+  const setMoveTop = () => {
+    const y = isTopLimit() ? 0 : options.y + 10;
+    setOptions({ x: options.x, y, scale: options.scale });
+  };
+
+  const setMoveBottom = () => {
+    const canvasHeight = canvasRef.current?.height || 0;
+    let y = isBottomLimit() ? canvasHeight - image.height * options.scale : options.y - 10;
+
+    setOptions({ x: options.x, y, scale: options.scale });
+  };
+
+  const setMoveRight = () => {
+    const x = isRightLimit() ? 0 : options.x + 10;
+
+    setOptions({ x, y: options.y, scale: options.scale });
+  };
+
+  const setMoveLeft = () => {
+    const canvasWidth = canvasRef.current?.width || 0;
+    let x = isLeftLimit() ? canvasWidth - image.width * options.scale : options.x - 10;
+
+    setOptions({ x, y: options.y, scale: options.scale });
+  };
+
+  const setImageAction = (action: ImageAction) => {
+    console.log('action: ', action);
+
+    switch (action) {
+      case ImageAction.ZoomIn:
+        setZoomIn();
+        break;
+      case ImageAction.ZoomOut:
+        setZoomOut();
+        break;
+      case ImageAction.Top:
+        setMoveTop();
+        break;
+      case ImageAction.Down:
+        setMoveBottom();
+        break;
+      case ImageAction.Right:
+        setMoveRight();
+        break;
+      case ImageAction.Left:
+        setMoveLeft();
+        break;
+    }
+  };
+
+  //   useEffect(() => {
+  //     const canvas = canvasRef.current;
+  //     const context = canvas.getContext(options.context || '2d');
+  //     let animationFrameId;
+
+  //     const render = () => {
+  //       draw(context);
+  //       animationFrameId = window.requestAnimationFrame(render);
+  //     };
+  //     render();
+
+  //     return () => {
+  //       window.cancelAnimationFrame(animationFrameId);
+  //     };
+  //   });
+
+  //   const draw = ctx => {
+  //     console.log('DRAW');
+  //   };
+
+  return { canvasRef, image, setImage, options, setOptions, setImageAction };
+};
+
+export default useCanvas;
